fc15fb18b18a9ebd20cd141b96e191553e55a9ea
Alex <sasha1235qaz@ukr.net>
2023-12-14 22:49:29 -0700

    optimize

---
+++ src/Header/index.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import s from "./header.module.css";
+
+
+export const Header = () => {
+    return (
+        <div className={s.header_wrap}>
+            <h2 className={s.main_text}>hello</h2>
+        </div>
+    )
+}
