d5f6f920f35b06f42f96535b70943592683fc5a6
Alex <o.d.bilyi@protonmail.com>
2024-01-21 20:41:25 +0100

    clear table components and use abortController

--- src/components/table/tablemain.jsx
+++ src/components/table/tablemain.jsx
@@ -5,6 +5,74 @@ import { getCryptoList } from "../../service/api";
 import { FilterIcon } from "../../icons/filterIcon";
 import { DropMenu } from "../dropDownBtnMenu";
 import { DownSvg } from "../../icons/downSvg";
+
+const formatMarketCap = (cap) => {
+  if (cap >= 1e12) {
+    return (
+      (cap / 1e12).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 4,
+        maximumFractionDigits: 3,
+      }) + " T"
+    );
+  } else if (cap >= 1e9) {
+    return (
+      (cap / 1e9).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3,
+      }) + " B"
+    );
+  } else if (cap >= 1e6) {
+    return (
+      (cap / 1e6).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 3,
+      }) + "M"
+    );
+  } else if (cap >= 1e3) {
+    return (
+      (cap / 1e3).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 1,
+        maximumFractionDigits: 3,
+      }) + "K"
+    );
+  } else {
+    return cap.toFixed().toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 1,
+      maximumFractionDigits: 5,
+    });
+  }
+};
+
+const createHashMapCoins = (coins) => {
+  return coins.reduce((acc, curr) => {
+    return {
+      ...acc,
+      [curr.name]: curr.rate,
+    };
+  }, {});
+};
+
+const formateRate = (coin) => {
+  return coin.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 1,
+    maximumFractionDigits: 2,
+  });
+};
+
+const filterType = ["price", "volume", "rank"];
+
 export const Table = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -14,8 +82,8 @@ export const Table = () => {
   const [dropMenuPosSort, setDropMenuPositionSort] = useState(null);
   const [sortFilter, setSortFilter] = useState("rank");
   const [orderFilter, setOrderFilter] = useState("ascending");
-  const [filterSortType , setFilterSortType] = useState([])
-  const filterType = ["price", "volume", "rank"];
+  const [filterSortType, setFilterSortType] = useState([]);
+
   const handleDropMenuClick = (event) => {
     setTimeout(() => {
       const btn = event.target.getBoundingClientRect();
@@ -40,120 +108,83 @@ export const Table = () => {
     showDropMenu(false);
     showDropMenuSort(false);
   };
-  const formatMarketCap = (cap) => {
-    if (cap >= 1e12) {
-      return (
-        (cap / 1e12).toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-          minimumFractionDigits: 4,
-          maximumFractionDigits: 3,
-        }) + " T"
-      );
-    } else if (cap >= 1e9) {
-      return (
-        (cap / 1e9).toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-          minimumFractionDigits: 3,
-          maximumFractionDigits: 3,
-        }) + " B"
-      );
-    } else if (cap >= 1e6) {
-      return (
-        (cap / 1e6).toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 3,
-        }) + "M"
-      );
-    } else if (cap >= 1e3) {
-      return (
-        (cap / 1e3).toLocaleString("en-US", {
-          style: "currency",
-          currency: "USD",
-          minimumFractionDigits: 1,
-          maximumFractionDigits: 3,
-        }) + "K"
-      );
-    } else {
-      return cap.toFixed().toLocaleString("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 1,
-        maximumFractionDigits: 5,
-      });
-    }
-  };
+
   const handleFilter = (filterItem) => {
     if (filterItem === "volume") {
       setSortFilter("volume");
-      getCurrentData();
-      setFilterSortType(["ascending","descending"])
+
+      setFilterSortType(["ascending", "descending"]);
     } else if (filterItem === "rank") {
       setOrderFilter("ascending");
       setSortFilter("rank");
-      getCurrentData();
-      setFilterSortType(["ascending"])
+
+      setFilterSortType(["ascending"]);
     } else if (filterItem === "price") {
       setSortFilter("price");
-      getCurrentData();
-      setFilterSortType(["ascending","descending"])
+
+      setFilterSortType(["ascending", "descending"]);
     }
     showDropMenu(false);
   };
   const handleOrderFilter = (orderFilter) => {
     if (orderFilter === "ascending") {
       setOrderFilter("ascending");
-      getCurrentData();
     } else if (orderFilter === "descending") {
       setOrderFilter("descending");
-      getCurrentData();
     }
     showDropMenuSort(false);
   };
-  const getCurrentData = useCallback(async () => {
-    try {
-      const oldInfo = JSON.parse(localStorage.getItem("cryptoHistory"));
-      const res = await getCryptoList(sortFilter, orderFilter);
-      if (oldInfo) {
-        const updatedInfo = res.map((newItem) => {
-          const oldItem = oldInfo.find((item) => item.name === newItem.name);
-          const checkRateChange = oldItem && newItem.rate > oldItem.rate;
-          return {
-            ...newItem,
-            up: checkRateChange,
-            formatedRate: newItem.rate.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD",
-              minimumFractionDigits: 1,
-              maximumFractionDigits: 2,
-            }),
-            formatedCap:
-              newItem.cap !== null ? formatMarketCap(newItem.cap) : "-",
-            formatedVolume:
-              newItem.volume !== null ? formatMarketCap(newItem.volume) : "-",
-          };
-        });
-        setCoins(updatedInfo);
+  const getCurrentData = useCallback(
+    async ({ sortFilter, orderFilter, signal }) => {
+      try {
+        const savedCoins = JSON.parse(localStorage.getItem("cryptoHistory"));
+        const res = await getCryptoList(sortFilter, orderFilter, signal);
+
+        if (Object.keys(savedCoins)?.length) {
+          const updatedInfo = res.map((newItem) => {
+            const oldItem = savedCoins[newItem.name];
+
+            return {
+              ...newItem,
+              up: newItem.rate > oldItem,
+              formatedRate: formateRate(newItem.rate),
+              formatedCap:
+                newItem.cap !== null ? formatMarketCap(newItem.cap) : "-",
+              formatedVolume:
+                newItem.volume !== null ? formatMarketCap(newItem.volume) : "-",
+            };
+          });
+          setCoins(updatedInfo);
+        }
+        localStorage.setItem(
+          "cryptoHistory",
+          JSON.stringify(createHashMapCoins(res))
+        );
+      } catch (error) {
+        console.error("Error fetching crypto data:", error);
+      } finally {
+        setLoading(false);
       }
-      localStorage.setItem("cryptoHistory", JSON.stringify(res));
-    } catch (error) {
-      console.error("Error fetching crypto data:", error);
-    } finally {
-      setLoading(false);
-    }
-  }, [sortFilter, orderFilter]);
+    },
+    []
+  );
+
   useEffect(() => {
-    getCurrentData();
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    getCurrentData({ sortFilter, orderFilter, signal });
+
     const apiCall = setInterval(() => {
-      getCurrentData();
-    }, 1000);
+      getCurrentData({ sortFilter, orderFilter, signal });
+    }, 20000);
+    
     return () => {
+      controller.abort();
       clearInterval(apiCall);
     };
-  }, [getCurrentData]);
+  }, [getCurrentData, sortFilter, orderFilter]);
+
   return (
     <div add={dropMenu ? true : false} className={t.table_container}>
       <div className={t.cryptocurrency_nav}>
@@ -167,14 +198,14 @@ export const Table = () => {
             closeDropDownMenu={handleCloseDropDownMenu}
             selectFilter={handleFilter}
             typeWhite
-          ></DropMenu>
+          />
         )}
         <div className={t.filterIcon}>
           <FilterIcon
             style={dropMenu ? { width: "calc(45px * 2)" } : null}
             className={t.filter_icon}
             onClick={(event) => handleDropMenuClick(event)}
-          ></FilterIcon>
+          />
         </div>
         {dropMenuPosSort?.top && dropMenuSort ? (
           <DropMenu
@@ -186,7 +217,7 @@ export const Table = () => {
             closeDropDownMenu={handleCloseDropDownMenu}
             selectFilter={handleOrderFilter}
             typeWhite
-          ></DropMenu>
+          />
         ) : null}
         <button
           onClick={(event) => handleDropMenuClickSort(event)}
--- src/service/api.js
+++ src/service/api.js
@@ -1,12 +1,13 @@
 const apiUrlList = "https://api.livecoinwatch.com/coins/list";
 const accessKey = "fcb4bb0c-76c7-4d5c-bd66-591451975a29";
-export const getCryptoList = async (setSort , setRank) => {
+export const getCryptoList = async (setSort , setRank, signal) => {
   const requestOptions = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "x-api-key": accessKey,
     },
+    signal: signal,
     body: JSON.stringify({
       currency: "USD",
       sort: setSort,
